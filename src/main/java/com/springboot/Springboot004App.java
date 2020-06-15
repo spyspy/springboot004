1d993bb9d3d1f973eafbf77aa717d97519d9fa08
Nelson <spyhopping101@gmail.com>
2020-06-15 17:22:30 +0800

    2020-06-15 Upload Files to Local Disk C

--- src/main/java/com/springboot/Springboot004App.java
+++ src/main/java/com/springboot/Springboot004App.java
@@ -16,48 +16,15 @@ import org.thymeleaf.templatemode.TemplateMode;
 //@Configuration
 //@ComponentScan
 //@SpringBootApplication(scanBasePackageClasses = {SpringBoot004Controller.class, FileUploadController.class})
+//Not Necessary use "scanBasePackageClasses" to specify the controller.
 @SpringBootApplication
 public class Springboot004App {
 
 	public static void main(String[] args) {
-		System.out.println("Let's GOOOO!!!!---------STRAT");
+		System.out.println("Let's GO APP  !!------STRAT");
 		SpringApplication.run(Springboot004App.class, args);
-		System.out.println("Let's HOMEHOME!!!!-----END");
+		System.out.println("Let's END APP !!------END");
 	}
-
-
-
-//	@Bean
-//	public SpringResourceTemplateResolver templateResolver(){
-//		// SpringResourceTemplateResolver automatically integrates with Spring's own
-//		// resource resolution infrastructure, which is highly recommended.
-//		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-//		templateResolver.setApplicationContext(this.applicationContext);
-//		templateResolver.setPrefix("/webapp/templates/");
-//		templateResolver.setSuffix(".html");
-//		// HTML is the default value, added here for the sake of clarity.
-//		templateResolver.setTemplateMode(TemplateMode.HTML);
-//		// Template cache is true by default. Set to false if you want
-//		// templates to be automatically updated when modified.
-//		templateResolver.setCacheable(true);
-//		return templateResolver;
-//	}
-//
-//	@Bean
-//	public SpringTemplateEngine templateEngine(){
-//		// SpringTemplateEngine automatically applies SpringStandardDialect and
-//		// enables Spring's own MessageSource message resolution mechanisms.
-//		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//		templateEngine.setTemplateResolver(templateResolver());
-//		// Enabling the SpringEL compiler with Spring 4.2.4 or newer can
-//		// speed up execution in most scenarios, but might be incompatible
-//		// with specific cases when expressions in one template are reused
-//		// across different data types, so this flag is "false" by default
-//		// for safer backwards compatibility.
-//		templateEngine.setEnableSpringELCompiler(true);
-//		return templateEngine;
-//	}
-
 }
 
 /**
--- src/main/java/com/springboot/controller/FileUploadController.java
+++ src/main/java/com/springboot/controller/FileUploadController.java
@@ -2,18 +2,52 @@ package com.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import java.io.*;
 
 @Controller
 public class FileUploadController {
+
+    private static String UPLOADED_FOLDER = ".//upload//";
+
     //2020/06/12 upload Test
-    @RequestMapping("/uploadFiles")
+    @GetMapping("/uploadFiles")
     public String listUploadedFiles(Model model) throws IOException {
 
-        System.out.println("Entering listUploadedFiles.......");
+        System.out.println("Entering (GET) listUploadedFiles.......");
 
         return "uploadFiles";
     }
+
+
+    @PostMapping("/uploadFiles")
+    public String sendPostUploadFiles(Model model,@RequestParam("file") MultipartFile multipartFile) throws IOException {
+
+        System.out.println("Entering (POST) sendPostUploadFiles.......");
+        System.out.println("multipartFile.getName(): "+multipartFile.getName() +" " +multipartFile.getOriginalFilename());
+
+        saveUploadedFiles(multipartFile);
+
+        return "redirect:/uploadFiles";
+    }
+
+    //Save Files From  Web page to Disk C:/test/
+    private void saveUploadedFiles(MultipartFile file) throws IOException {
+        InputStream initialStream = file.getInputStream();
+
+        byte[] buffer = new byte[initialStream.available()];
+        initialStream.read(buffer);
+        File targetFile = new File("C://test//"+file.getOriginalFilename());
+        OutputStream outStream = new FileOutputStream(targetFile);
+        outStream.write(buffer);
+        outStream.close();
+
+    }
+
+    //Save Files From  Web page to Server /uploadFiles/
+
 }
